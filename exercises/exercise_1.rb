9086f1676ad1a64e0b32a0b4a09cc87f5cdd00a4
Aasem Rahuma <aasem.rahuma@gmail.com>
2023-07-25 14:36:08 -0400

    finished excercises 1 to 4

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,11 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+# Creating the three stores using the create method
+Burnaby = Store.create(name: 'Burnaby', annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+Richmond = Store.create(name: 'Richmond', annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+Gastown = Store.create(name: 'Gastown', annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+# Output the number of stores using the count method
+puts Store.count
