14d4fec33ccddbd6bda8ea52ca2ff85d28399bd9
Aasem Rahuma <aasem.rahuma@gmail.com>
2023-07-25 15:39:11 -0400

    finishes excercises from 5 to 7

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@total_revenue = Store.sum(:annual_revenue)
+@average_revenue = Store.average(:annual_revenue)
+
+@milli = Store.where('annual_revenue > 1000000').count
+
+puts @sum
+puts @aver
+puts @mill
